ecc8147aca2b51367f59cf8adf99882afec1201b
Georgy Makarov <60594767+GeorgyMakarov@users.noreply.github.com>
2021-01-21 15:31:22 +0100

    add distribution generator function

--- distr_generator.R
+++ distr_generator.R
@@ -3,7 +3,7 @@
 # Params s1 and s2 change the skeweness and form of distribution
 # @name     f.dpl
 # @param    rd  seed for reproducibility
-# @param    nb   number of observations
+# @param    nb  number of observations
 # @param    s1  shape1 of beta distribution
 # @param    s2  shape2 of beta distribution
 # @param    mf  multiplication factor to convert distribution to units of choice
@@ -18,21 +18,36 @@ f.dpl = function(rd, nb, s1, s2, mf) {
 }
 
 
+# Generates distribution with given parameters
+# Use to generate distributions for integer / decimal variables
+# Params s1 and s2 change the skeweness and form of distribution
+# @name     f.dgn
+# @param    rd  seed for reproducibility
+# @param    nb  number of observations
+# @param    s1  shape1 of beta distribution
+# @param    s2  shape2 of beta distribution
+# @param    mf  multiplication factor to convert distribution to units of choice
+# @param    ro  should round up the values if TRUE, default FALSE
+# @return   density plot
+# @export
+f.dgn = function(rd, nb, s1, s2, mf, ro = FALSE) {
+    set.seed(rd)
+    x = rbeta(n = nb, shape1 = s1, shape2 = s2) * mf
+    if (ro == TRUE) {
+        x = round(x, 0)
+    }
+    x
+}
+
+
 # Examples
 
-## generate number of patients waiting in a queue with right skew
-f.dpl(3, 100000, 2, 20, 10)
-
-## generate number of patients waiting in a queue with left skew
-f.dpl(3, 100000, 20, 2, 10)
-
-set.seed(3)
-x = rbeta(100000, 20, 2)
-x = x *10
-x = round(x, 0)
-head(x)
-summary(x)
-hist(x)
-
-# TO DO: add distribution generator with an option to round the result to obtain
-#        integer values. Lines 29 -- 35 show how to approach this
+# ## generate number of patients waiting in a queue with right skew
+# f.dpl(3, 100000, 2, 20, 10)                 # plot distribution
+# rskew = f.dgn(3, 100000, 2, 20, 10, T)      # generate distribution
+# hist(rskew, col = "lightgreen", breaks = 5) # plot and compare to f.dpl
+# 
+# ## generate number of patients waiting in a queue with left skew
+# f.dpl(3, 100000, 20, 2, 10)                 # plot distribution
+# lskew = f.dgn(3, 100000, 20, 2, 10, T)      # generate distribution
+# hist(lskew, col = "lightgreen", breaks = 5) # plot and compare to f.dpl
