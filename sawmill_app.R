add759d74894d1c169883c0b886666715f82ddb3
Georgy Makarov <60594767+GeorgyMakarov@users.noreply.github.com>
2021-01-25 13:18:10 +0100

    create shiny app

---
+++ sawmill_app.R
@@ -0,0 +1,46 @@
+setwd("/home/georgy/Документы/GitHub/discrete_event_sim/")
+suppressPackageStartupMessages(library(shiny))
+suppressPackageStartupMessages(library(dplyr))
+suppressPackageStartupMessages(library(waiter))
+suppressPackageStartupMessages(library(shinythemes))
+
+
+rm(list = ls())
+source("distr_generator.R")
+source("./modules/simpleq.R")
+source("./modules/sawnmod.R")
+
+
+ui = fluidPage(
+    theme = shinytheme("united"),
+    navbarPage(
+        "Discrete event simulator",
+        tabPanel("Simple queue",
+                 br(),
+                 sidebarLayout(
+                     sidebarPanel(
+                         simplemoduleUI("simpleq")
+                     ),
+                     mainPanel(
+                         plotOutput("simpleqplot")
+                     )
+                 )),
+        tabPanel("Sawmill sim",
+                 br(),
+                 sidebarLayout(
+                     sidebarPanel(
+                         sawmoduleUI("sawn")
+                     ),
+                     mainPanel(
+                         plotOutput("sawnplot")
+                     )
+                 )),
+        tabPanel("About",
+                 br())
+    )
+)
+
+
+server = function(input, output, session) {}
+
+shinyApp(ui, server)
